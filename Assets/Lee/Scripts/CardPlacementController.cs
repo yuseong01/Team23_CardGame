7d7472d56342fb50ad6ec00f713507a5375e1f70
lumos <206481784+lumos-gna@users.noreply.github.com>
2025-04-08 19:26:06 +0900

    feat : 멤버별 스프라이트를 카테고리 별로 선택할수있게 변경

--- Assets/Lee/Scripts/CardPlacementController.cs
+++ Assets/Lee/Scripts/CardPlacementController.cs
@@ -19,20 +19,74 @@ public class CardPlacementController : MonoBehaviour
     [SerializeField] RectTransform cardParentRectTransform;
 
     
-    //게임 시작시 카드배치, 인덱스 반환
-    public int[] InitStartGame(int levelValue, int memberCount, GameObject cardPrefab)
+    public List<Cards> StartCardPlacement(int levelValue, MemberSpritesContainer memberSpritesContainer, Cards cardPrefab)
     {
         var leveledColumnCount = defalutColumnCount + levelValue;
 
         var totalCardCount = leveledColumnCount * (leveledColumnCount - 1);
 
 
-        SetCellSize(leveledColumnCount, cardPrefab.GetComponent<RectTransform>());
+        SetCellSize(leveledColumnCount, cardPrefab.frontImage.rectTransform);
 
-        EnableCards(totalCardCount, cardPrefab);
 
-        return GetCardRandCells(totalCardCount, memberCount);
+
+        List<(int, int)> cardKeyList = GetCardKeyList(totalCardCount, memberSpritesContainer);
+
+        return InitCardTable(totalCardCount, cardKeyList, memberSpritesContainer, cardPrefab);
     }
+
+    List<(int, int)> GetCardKeyList(int totalCardCount, MemberSpritesContainer memberSpritesContainer)
+    {
+        int memberIndex = 0;
+
+        int pairCount = 2;
+
+        List<(int, int)> cardKeyList = new();
+
+        for (int i = 0; i < totalCardCount; i += pairCount)
+        {
+            if (memberIndex % memberSpritesContainer.totalMemberCount == 0)
+            {
+                memberIndex = 0;
+            }
+
+            int randCategoryNum = Random.Range(0, memberSpritesContainer.CategoryCount);
+
+            for (int j = 0; j < pairCount; j++)
+            {
+                cardKeyList.Add(new(randCategoryNum, memberIndex));
+            }
+
+            memberIndex++;
+        }
+
+        return cardKeyList.OrderBy(x=> Random.value).ToList();
+    }
+
+    List<Cards> InitCardTable(int totalCardCount, List<(int,int)> cardKeyList, MemberSpritesContainer memberSpritesContainer, Cards cardPrefab)
+    {
+        var memberSpriteList = memberSpritesContainer.spritesList;
+
+        List<Cards> totalCardsList = new();
+
+        for (int i = 0; i < totalCardCount; i ++)
+        {
+            var targetCard = cardParentRectTransform.childCount <= i ?
+               Instantiate(cardPrefab, cardParentRectTransform) :
+               cardParentRectTransform.GetChild(i).GetComponent<Cards>();
+
+            cardObejctQue.Enqueue(targetCard.gameObject);
+
+            targetCard.gameObject.SetActive(true);
+
+            targetCard.Init(cardKeyList[i], memberSpriteList[cardKeyList[i].Item1][cardKeyList[i].Item2]);
+
+            totalCardsList.Add(targetCard);
+        }
+
+        return totalCardsList;
+    }
+
 
 
     //게임 종료시 카드비활성화
@@ -41,41 +95,6 @@ public class CardPlacementController : MonoBehaviour
         DisableCards();
     }
 
-
-
-    int[] GetCardRandCells(int totalCardCount, int memberCount)
-    {
-        int memberIndex = 0;
-
-        List<int> memberIndexList = new();
-
-        List<int> memberRandCellList = new();
-
-        for (int i = 0; i < memberCount; i++)
-        {
-            memberIndexList.Add(i);
-        }
-
-
-        for (int i = 0; i < totalCardCount; i+= 2)
-        {
-            if (memberIndex == memberIndexList.Count || i == 0)
-            {
-                memberIndex = 0;
-
-                memberIndexList = memberIndexList.OrderBy(x => Random.value).ToList();
-            }
-
-            for (int j = 0; j < 2; j++)
-            {
-                memberRandCellList.Add(memberIndexList[memberIndex]);
-            }
-
-            memberIndex++;
-        }
-
-        return memberRandCellList.OrderBy(x => Random.value).ToArray();
-    }
 
 
     void SetCellSize(int leveledColumnCount ,RectTransform targetRect)
@@ -89,20 +108,6 @@ public class CardPlacementController : MonoBehaviour
         cardParentGrid.spacing = gridCellSpacing;
     }
 
-    void EnableCards(int totalCardCount, GameObject cardPrefab)
-    {
-        for (int i = 0; i < totalCardCount; i++)
-        {
-            var targetCardObject = cardParentRectTransform.childCount <= i ?
-                Instantiate(cardPrefab.gameObject, cardParentRectTransform) :
-                cardParentRectTransform.GetChild(i).gameObject;
-
-            targetCardObject.SetActive(true);
-
-            cardObejctQue.Enqueue(targetCardObject);
-        }
-    }
-
     void DisableCards()
     {
         while (cardObejctQue.Count > 0)
@@ -110,4 +115,5 @@ public class CardPlacementController : MonoBehaviour
             cardObejctQue.Dequeue().SetActive(false);
         }
     }
+
 }
---
+++ Assets/Lee/Scripts/MemberSpritesContainer.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class MemberSpritesContainer 
+{
+    public List<Sprite[]> spritesList;
+
+
+    public int totalMemberCount;
+    public int CategoryCount => spritesList.Count;
+
+    public void Init(int totalMemberCount)
+    {
+        spritesList = new();
+
+        this.totalMemberCount = totalMemberCount;
+    }
+
+    public void AddSprites(Sprite[] addSprites)
+    {
+        if(addSprites.Length == totalMemberCount)
+        {
+            spritesList.Add(addSprites);
+        }
+    }
+}
