44fe271fbb37df0f2fe15a8f140d6d120851e0ae
FlamingBun <wh180911@gmail.com>
2025-04-10 11:56:52 +0900

    refactor: ClearUI 수정

--- Assets/Han/Scripts/Profile.cs
+++ Assets/Han/Scripts/Profile.cs
@@ -5,18 +5,40 @@ using UnityEngine.UI;
 
 public class Profile : MonoBehaviour
 {
+    [Header("UI")]
+    public Image frameImage;
     public Image image;
     public Text nameTxt;
 
-    public void SetProfile()
+    [Space(10)]
+    [Header("ClearImage")]
+    [SerializeField] private Sprite[] profileImages;
+
+    [Space(10)]
+    [Header("FailImage")]
+    [SerializeField] private Sprite[] failImages;
+
+    // Fail
+    public void SetProfile(Color color)
     {
-        image.sprite = Resources.Load<Sprite>($"fail");
+        frameImage.color = color;
+        int randomIdx = Random.Range(0, failImages.Length);
+        image.sprite = failImages[randomIdx];
         nameTxt.text = "";
     }
 
+    // Clear - 2번 이상 호출됐을 때 -> 배경 색상 변경 x
     public void SetProfile(int idx, string name)
     {
-        image.sprite = Resources.Load<Sprite>($"profile{idx}");
+        image.sprite = profileImages[idx];
+        nameTxt.text = name;
+    }
+
+    // Clear 처음 호출될 때 -> 배경 색상 변경
+    public void SetProfile(int idx, string name, Color color)
+    {
+        frameImage.color = color;
+        image.sprite = profileImages[idx];
         nameTxt.text = name;
     }
 }
