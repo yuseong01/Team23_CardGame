a2df452a6ee90558b7efba98bc187565aa4c7736
유성(yuseong) <62369592+yuseong01@users.noreply.github.com>
2025-04-10 11:38:45 +0900

    feat: 카드들 보드에까는소리, 카드선택소리추가

--- Assets/Yuseong/Scripts/SoundManager.cs
+++ Assets/Yuseong/Scripts/SoundManager.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class SoundManager : MonoBehaviour
 {
     [SerializeField] AudioSource bgmAudioSource;
-    [SerializeField] AudioSource flipSoundAudioSource;
+    [SerializeField] AudioSource CardSoundAudioSource;
 
     //unity변수에 들어갈부분, 다른곳에서 요청하는 각각의 해당하는 오디오소스의 교체를 위해
-    [SerializeField] private AudioClip flipSuccessSoundClip;    //설정
-    [SerializeField] private AudioClip flipFailSoundClip;    //설정 
-    [SerializeField] private AudioClip gameBGMClip;    //설정
+    [SerializeField] private AudioClip flipSuccessSoundClip;    
+    [SerializeField] private AudioClip flipFailSoundClip;     
+    [SerializeField] private AudioClip gameBGMClip;    
+    [SerializeField] private AudioClip setCardClip;
+    [SerializeField] private AudioClip touchCardClip;
+
+
 
     public void PlayGameBGM()
     {
@@ -19,12 +23,22 @@ public class SoundManager : MonoBehaviour
     }
     public void PlayFlipSuccessSound()  //나중에는 clip을 매개변수로 받아와서 함수를 하나만 만들어도 됨
     {
-        flipSoundAudioSource.clip = flipSuccessSoundClip;
-        flipSoundAudioSource.Play();
+        CardSoundAudioSource.clip = flipSuccessSoundClip;
+        CardSoundAudioSource.Play();
     }
     public void PlayFlipFailSound()
     {
-        flipSoundAudioSource.clip = flipFailSoundClip;
-        flipSoundAudioSource.Play();
+        CardSoundAudioSource.clip = flipFailSoundClip;
+        CardSoundAudioSource.Play();
+    }
+    public void PlaySetCardSound()
+    {
+        CardSoundAudioSource.clip = setCardClip;
+        CardSoundAudioSource.Play();
+    }
+    public void PlayTouchCardSound()
+    {
+        CardSoundAudioSource.clip = flipFailSoundClip;
+        CardSoundAudioSource.Play();
     }
 }
